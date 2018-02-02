c8a59859b7205d198df72b5aeebab5939e23daa2
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-02-02 16:27:29 +0700

    day 3

---
+++ Bank Costumer Churn Prediction/main.py
@@ -0,0 +1,54 @@
+# Artificial Neural Network
+
+# Data Preprocessing
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv("Churn_Modelling.csv")
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:,1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:,2])
+
+onehotencoder = OneHotEncoder(categorical_features=[1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# Part 2 - Building the ANN!
+
+# Improting the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initializing the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units=6, activation="relu", input_dim=11))
+
+# Adding the second hidden layer
+classifier.add(Dense(units=6, activation="relu"))
+
+# Adding the output layer
+classifier.add(Dense(units=1, activation="sigmoid"))
+
+# Compiling the ANN
+classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, )
